f53a4e2f7aa55e1b4c75dd9278fd12ff7eb470df
IrynaKr1 <krutsiryna@gmail.com>
2026-04-20 19:27:54 +0200

    Add phone controller basics

---
+++ controller/phone.controller.js
@@ -0,0 +1,10 @@
+const createHttpError = require('http-errors');
+const mongoose = require('mongoose');
+
+module.exports.createPhone = async (req, res, next) => {};
+module.exports.getPhones = async (req, res, next) => {
+};
+
+module.exports.getPhoneById = async (req, res, next) => {};
+module.exports.updatePhoneById = async (req, res, next) => {};
+module.exports.deletePhoneById = async (req, res, next) => {};
