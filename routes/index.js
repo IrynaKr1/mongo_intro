c786b20fcc745fed734fb07b7682694f8ee70ad5
IrynaKr1 <krutsiryna@gmail.com>
2026-04-20 19:27:39 +0200

    Add phone routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const userRouter = require('./userRouter');
 const postRouter = require('./postRouter');
+const phonesRouter = require('./phonesRouter')
 
 const router = Router();
 
 router.use('/users', userRouter);
 router.use('/posts', postRouter);
+router.use('/phones', phonesRouter);
 
 module.exports = router;
